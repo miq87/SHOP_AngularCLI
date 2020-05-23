adbbf934f0654d982c77901074c790f0085584c2
MIQ87 <miq3l@wp.pl>
2020-05-23 22:04:17 +0200

    ok

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,17 +1,17 @@
 export class Product {
-    _id: number;
-    name: string;
-    description: string;
-    price: number;
-    imgUrl: string;
+    _id: number
+    name: string
+    description: string
+    price: number
+    imgUrl: string
 
     constructor(_id, name, description = '', price = 0,
     imgUrl = 'https://5.imimg.com/data5/BX/VE/MY-9324435/dorna-ac-drive-500x500.jpg') {
-        this._id = _id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imgUrl = imgUrl;
+        this._id = _id
+        this.name = name
+        this.description = description
+        this.price = price
+        this.imgUrl = imgUrl
     }
 
 }
