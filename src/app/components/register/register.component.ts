9c6552acddf6515e223b82cc2834493893c501f1
miq87 <miq3l@wp.pl>
2020-06-04 14:53:13 +0200

    new user model, auth service

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +11,20 @@ export class RegisterComponent implements OnInit {
 
   registerUserData = <any>{}
 
+  registerValid = false
+
   constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onSubmit() {
-    console.log(this.registerUserData)
+    let body = JSON.stringify(this.registerUserData)
+    let bodyJSON = JSON.parse(body)
+
+    this.authService.registerUser(bodyJSON).subscribe(data => {
+      this.registerValid = true
+      console.log(data)
+    })
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -12,7 +12,7 @@ export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  registerUser(userData: User): Observable<any> {
+  registerUser(userData: any): Observable<any> {
     return this.http.post(registerUrl, userData)
   }
 
