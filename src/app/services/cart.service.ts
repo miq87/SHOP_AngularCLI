59b520f3c19d0825c33e60be8dc7a97209f35f90
miq87 <miq3l@wp.pl>
2020-06-18 11:01:37 +0200

    autoupdate

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -15,26 +15,6 @@ export class CartService {
 
   getCartItems(): Observable<CartItem[]> {
     return this.http.get<CartItem[]>(cartUrl)
-    // .pipe(
-    //   map((result : CartItem[]) => {
-    //     let cartItems : CartItem[] = []
-    //     for(let item of result) {
-    //       let productExists = false;
-
-    //       for(let i in cartItems) {
-    //         if(cartItems[i].productId === item.productId) {
-    //           cartItems[i].qty++
-    //           productExists = true
-    //           break;
-    //         }
-    //       }
-    //       if(!productExists) {
-    //         cartItems.push(item)
-    //       }
-    //     }
-    //     return cartItems
-    //   })
-    // )
   }
   addProductToCart(product: Product): Observable<any> {
     let cartItem = new CartItem(product)
