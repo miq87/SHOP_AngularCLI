2f5e97d339885c8c54491bc9ebf38942da255e94
miq87 <miq3l@wp.pl>
2020-06-19 15:02:36 +0200

    autoupdate

--- src/app/services/token-interceptor.service.ts
+++ src/app/services/token-interceptor.service.ts
@@ -10,12 +10,14 @@ export class TokenInterceptorService implements HttpInterceptor {
   intercept(req, next) {
     let authService = this.injector.get(AuthService)
 
-    let tokenizedReq = req.clone({
+    /*let tokenizedReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${authService.getToken()}`
       }
     })
-
-    return next.handle(tokenizedReq)
+    */
+    req = req.clone({ headers: req.headers.set('Authorization', 'Bearer ' + authService.getToken()) });
+    console.log(req)
+    return next.handle(req)
   }
 }
