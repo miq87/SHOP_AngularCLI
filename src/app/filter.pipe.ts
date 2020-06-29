7fcf0d4ec52927d66e606f15678bf53b30702117
MIQ87 <miq3l@wp.pl>
2020-06-29 23:22:43 +0200

    autoupdate

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,11 +6,16 @@ import { Product } from './models/product';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, priceMin: number): any {
-    console.log(value)
-    console.log(priceMin)
-    //console.log(priceMax)
-    return value
-  }
+  produkty : Product[] = []
 
+  transform(value: Product[], ...args: any[]): any {    
+    //if(args[0] == 0 || args[1] == 0) return value
+
+    value.forEach(element => {
+      if(element.price > args[0] && element.price < args[1]) {
+        this.produkty.push(element)
+      }
+    });
+    return this.produkty
+  }
 }
